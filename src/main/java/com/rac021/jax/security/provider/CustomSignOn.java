020d1737f24904a8bdc752bfc28abd85693e808d
Rachid Yahiaoui <ryahiaoui@ORL-5CB3410DK4.orleans.inra.fr>
2017-06-26 17:06:09 +0200

    G-Jax-Security-Provider 1.1

--- src/main/java/com/rac021/jax/security/provider/CustomSignOn.java
+++ src/main/java/com/rac021/jax/security/provider/CustomSignOn.java
@@ -16,7 +16,7 @@ import java.security.NoSuchAlgorithmException ;
 import com.rac021.jax.api.qualifiers.security.Custom ;
 import com.rac021.jax.api.configuration.IConfigurator ;
 import com.rac021.jax.api.exceptions.BusinessException ;
-import com.rac021.jax.security.provider.configuration.Configurator ;
+import com.rac021.jax.security.provider.configuration.YamlConfigurator ;
 
 /**
  *
@@ -31,10 +31,15 @@ public class CustomSignOn implements ISignOn {
     private EntityManager entityManager ;
 
     @Inject
-    private Configurator configurator ;
+    private YamlConfigurator configurator ;
             
     public CustomSignOn() {
     }
+    
+    private final String SHA1   = "SHA1"  ;
+    private final String SHA2   = "SHA2"  ;
+    private final String MD5    = "MD5"   ;
+    private final String PLAIN  = "PLAIN" ;
     
     @Override
     public boolean checkIntegrity( String token, Long expiration ) throws BusinessException {
@@ -54,7 +59,9 @@ public class CustomSignOn implements ISignOn {
             long expiredTime  = now - expiration ;
             
             if( clientTime < expiredTime )  {
-                throw  new BusinessException(" Error : TimeStamp expired // -->   Expiration delay = " + expiration + " second(s) " ) ;
+                throw  new BusinessException( " Error : TimeStamp expired // -->  "
+                                             + "Expiration delay = " + expiration 
+                                             + " second(s) " ) ;
             }
         }
         
@@ -62,24 +69,34 @@ public class CustomSignOn implements ISignOn {
     }
     
     @Override
-    public boolean checkIntegrity(String _login, String _timeStamp, String _clientSign) throws BusinessException {
+    public boolean checkIntegrity( String _login       ,  
+                                   String _timeStamp   , 
+                                   String _clientSign  ) throws BusinessException {
         
        String tableName          = configurator.getTableName()          ;
        String loginColumnName    = configurator.getLoginColumnName()    ;
        String passwordColumnName = configurator.getPasswordColumnName() ;
        
-       Query query = entityManager.createNativeQuery( " SELECT " + passwordColumnName + " FROM " + tableName + 
-                                                      " WHERE "  + loginColumnName +" = '" + _login + "'" )  ;
+       Query query = entityManager.createNativeQuery( " SELECT " + passwordColumnName + 
+                                                      " FROM " + tableName            + 
+                                                      " WHERE "  + loginColumnName    +
+                                                      " = '" + _login + "'" )         ;
             
        if( tableName == null || loginColumnName == null || passwordColumnName == null ) {
          if( tableName == null ) {
-             throw  new BusinessException( " tableName Can't be NULL. May be this information is missing in the serviceConf yaml !! " ) ;
+             throw  new BusinessException( " tableName Can't be NULL. "
+                                            + "May be this information is missing in "
+                                            + "the serviceConf yaml !! " ) ;
          }
          if( loginColumnName == null ) {
-             throw  new BusinessException( " loginColumnName Can't be NULL. May be this information is missing in the serviceConf yaml !! " ) ;
+             throw  new BusinessException( " loginColumnName Can't be NULL. "
+                                            + "May be this information is missing "
+                                            + "in the serviceConf yaml !! " ) ;
          }
          if( passwordColumnName == null ) {
-             throw  new BusinessException( " passwordColumnName Can't be NULL. May be this information is missing in the serviceConf yaml !! " ) ;
+             throw  new BusinessException( " passwordColumnName Can't be NULL. "
+                                           + "May be this information is missing "
+                                           + "in the serviceConf yaml !! " ) ;
          }
        }
        
@@ -100,74 +117,126 @@ public class CustomSignOn implements ISignOn {
         
         // Treat Strategy 
         
-        if( loginSignature.equalsIgnoreCase("SHA1")) {
+        if( loginSignature.equalsIgnoreCase(SHA1)) {
            try {
-               login = Digestor.digestSha1( _login ) ;
+               login = Digestor.toString(Digestor.toSHA1(_login )) ;
            } catch (NoSuchAlgorithmException ex) {
-               Logger.getLogger(CustomSignOn.class.getName()).log(Level.SEVERE, null, ex);
+               Logger.getLogger( CustomSignOn.class.getName())
+                                             .log(Level.SEVERE, null, ex) ;
            }
         }       
         
-        else if( loginSignature.equalsIgnoreCase("MD5")) {
+        else if( loginSignature.equalsIgnoreCase(MD5)) {
            try {
-               login = Digestor.digestMD5(_login ) ;
+               login =  Digestor.toString( Digestor.toMD5(_login )) ;
            } catch (NoSuchAlgorithmException ex) {
-               Logger.getLogger(CustomSignOn.class.getName()).log(Level.SEVERE, null, ex) ;
+               Logger.getLogger( CustomSignOn.class.getName())
+                                             .log(Level.SEVERE, null, ex) ;
+           }
+        }   
+        
+        else if( loginSignature.equalsIgnoreCase(SHA2)) {
+           try {
+               login =  Digestor.toString( Digestor.toSHA256(_login )) ;
+           } catch (NoSuchAlgorithmException ex) {
+               Logger.getLogger( CustomSignOn.class.getName())
+                                                   .log(Level.SEVERE, null, ex ) ;
            }
         }       
         
-        if ( passwordSignature.equalsIgnoreCase("PLAIN") && storedPassword.equalsIgnoreCase("SHA1")  ||
-             passwordSignature.equalsIgnoreCase("SHA1")  && storedPassword.equalsIgnoreCase("PLAIN")  ) {
+        if ( passwordSignature.equalsIgnoreCase(PLAIN) && 
+             storedPassword.equalsIgnoreCase(SHA1)     ||
+             passwordSignature.equalsIgnoreCase(SHA1)  && 
+             storedPassword.equalsIgnoreCase(PLAIN)  )  {
            try {
-               password = Digestor.digestSha1( storedPassword ) ;
+               password =  Digestor.toString( Digestor.toSHA1( storedPassword )) ;
            } catch (NoSuchAlgorithmException ex) {
-               Logger.getLogger(CustomSignOn.class.getName()).log(Level.SEVERE, null, ex) ;
+               Logger.getLogger( CustomSignOn.class.getName())
+                                             .log(Level.SEVERE, null, ex ) ;
            }
         }
         
-        else if ( passwordSignature.equalsIgnoreCase("PLAIN") && storedPassword.equalsIgnoreCase("MD5") ||
-             passwordSignature.equalsIgnoreCase("MD5") && storedPassword.equalsIgnoreCase("PLAIN")  ) {
+        else if ( passwordSignature.equalsIgnoreCase(PLAIN) && 
+                  storedPassword.equalsIgnoreCase(MD5)      ||
+                  passwordSignature.equalsIgnoreCase(MD5)   && 
+                  storedPassword.equalsIgnoreCase(PLAIN)  )  {
            try {
-               password = Digestor.digestMD5(storedPassword ) ;
+               password =  Digestor.toString ( Digestor.toMD5(storedPassword ) ) ;
            } catch (NoSuchAlgorithmException ex) {
-               Logger.getLogger(CustomSignOn.class.getName()).log(Level.SEVERE, null, ex) ;
+               Logger.getLogger( CustomSignOn.class.getName())
+                                             .log(Level.SEVERE, null, ex ) ;
+           }
+        }
+        else if ( passwordSignature.equalsIgnoreCase(PLAIN) && 
+                  storedPassword.equalsIgnoreCase(SHA2)     ||
+                  passwordSignature.equalsIgnoreCase(SHA2)  && 
+                  storedPassword.equalsIgnoreCase(PLAIN)  )  {
+           try {
+               password =  Digestor.toString ( Digestor.toSHA256(storedPassword ) ) ;
+           } catch (NoSuchAlgorithmException ex) {
+               Logger.getLogger( CustomSignOn.class.getName())
+                                             .log(Level.SEVERE, null, ex) ;
            }
         }
         
-        if( timeStampSiignature.equalsIgnoreCase("SHA1")) {
+        if( timeStampSiignature.equalsIgnoreCase(SHA1)) {
            try {
-               timeStamp = Digestor.digestSha1( _timeStamp ) ;
+               timeStamp =  Digestor.toString ( Digestor.toSHA1(_timeStamp )) ;
            } catch (NoSuchAlgorithmException ex) {
-               Logger.getLogger(CustomSignOn.class.getName()).log(Level.SEVERE, null, ex) ;
+               Logger.getLogger( CustomSignOn.class.getName())
+                                                   .log(Level.SEVERE, null, ex) ;
            }
         }
-        else if( timeStampSiignature.equalsIgnoreCase("MD5")) {
+        else if( timeStampSiignature.equalsIgnoreCase(MD5)) {
            try {
-               timeStamp = Digestor.digestMD5(_timeStamp ) ;
+               timeStamp =  Digestor.toString ( Digestor.toMD5(_timeStamp ) ) ;
            } catch (NoSuchAlgorithmException ex) {
-               Logger.getLogger(CustomSignOn.class.getName()).log(Level.SEVERE, null, ex) ;
+               Logger.getLogger( CustomSignOn.class.getName())
+                                                   .log(Level.SEVERE, null, ex) ;
+           }
+        }
+        else if( timeStampSiignature.equalsIgnoreCase(SHA2)) {
+           try {
+               timeStamp =  Digestor.toString ( Digestor.toSHA256(_timeStamp ) ) ;
+           } catch (NoSuchAlgorithmException ex) {
+               Logger.getLogger( CustomSignOn.class.getName())
+                                                   .log(Level.SEVERE, null, ex) ;
            }
         }
               
         try {
             
-           if(algo.equalsIgnoreCase("SHA1")) {
-            String calculatedSign = Digestor.digestSha1( login + password + timeStamp ) ;
+           if(algo.equalsIgnoreCase(SHA1)) {
+            String calculatedSign =  Digestor.toString( Digestor.toSHA1( login        + 
+                                                                         password     + 
+                                                                         timeStamp )) ;
             if(calculatedSign.equals(_clientSign)) {
                  ISignOn.ENCRYPTION_KEY.set(password) ;
                 return true ; 
             }
            }
-           else if(algo.equalsIgnoreCase("MD5")) {
-            String calculatedSign = Digestor.digestMD5(login + password + timeStamp ) ;
-            if(calculatedSign.equals(_clientSign)) {
-                 ISignOn.ENCRYPTION_KEY.set(password) ;
+           else if(algo.equalsIgnoreCase(MD5)) {
+            String calculatedSign =  Digestor.toString ( Digestor.toMD5 ( login       + 
+                                                                          password    + 
+                                                                          timeStamp ) ) ;
+            if(calculatedSign.equals(_clientSign))   {
+                ISignOn.ENCRYPTION_KEY.set(password) ;
+                return true ; 
+            }
+           }
+           else if(algo.equalsIgnoreCase(SHA2)) {
+            String calculatedSign =  Digestor.toString ( Digestor.toSHA256( login       + 
+                                                                            password    + 
+                                                                            timeStamp ) ) ;
+            if(calculatedSign.equals(_clientSign))   {
+                ISignOn.ENCRYPTION_KEY.set(password) ;
                 return true ; 
             }
            }
            
         } catch (NoSuchAlgorithmException ex) {
-            Logger.getLogger(CustomSignOn.class.getName()).log(Level.SEVERE, null, ex) ;
+            Logger.getLogger(CustomSignOn.class.getName())
+                                         .log(Level.SEVERE, null, ex) ;
         }
 
         return false ;
